e31547b223ee89132287abea9748771be3d708d0
Doug Alderman <agnadcp-d1@yahoo.com>
2016-07-17 19:29:59 -0600

    work in progress picture answers

--- js/directives/pictureAnswer.js
+++ js/directives/pictureAnswer.js
@@ -16,25 +16,38 @@ angular.module('pictureQuiz')
             userAnswered: '=',
             userAnsweredCorrectly: '=',
             getNextQuestion: '&',
+			borderOn: '=',
             gotoTop: '&'
         },
         controller: function($scope) {
             $scope.userAnswered = false;
             $scope.userAnsweredCorrectly = false;
-            $scope.processUserInput = function(selection) {
+            $scope.processUserInput = function(selection, whereFrom) {
                 if (!$scope.userAnswered) { // if haven't already answered question
-                    
-                    $scope.userAnswered = true;
-                    if (selection === $scope.correctAnswer) {
-                        $scope.userCorrect[$scope.questionId] = true;
-                        $scope.userAnsweredCorrectly = true;
-                    }
-                    else {
-                        $scope.userCorrect[$scope.questionId] = false;
-                        $scope.userAnsweredCorrectly = false;
-                    }
-					$scope.gotoTop({numPixels: 0});
-                }
+                   if (selection) // if selection has value
+                        $scope.selection = $scope.options[selection].answer;
+                    var temp = ((whereFrom === 'fromSelect') && $scope.autoSubmit);
+                    var temp2 = ((whereFrom === 'fromSubmit') && !$scope.autoSubmit);
+                    if (temp || temp2) {
+                        $scope.userAnswered = true;
+                        if ($scope.selection === $scope.correctAnswer) {
+                            $scope.userCorrect[$scope.questionId] = true;
+                            $scope.userAnsweredCorrectly = true;
+                        }
+                        else {
+                            $scope.userCorrect[$scope.questionId] = false;
+                            $scope.userAnsweredCorrectly = false;
+                        }
+						$scope.gotoTop({numPixels: 0});
+                   }
+					else { // selected but not submitted / autosubmit
+						for (var i = 0; i < $scope.options.length; i++) {
+							$scope.borderOn[i] = false; // initialize to false for all options
+						}
+						$scope.answer = selection;
+						$scope.borderOn[selection] = true;
+					}
+                }    
             }    
         }
     }
